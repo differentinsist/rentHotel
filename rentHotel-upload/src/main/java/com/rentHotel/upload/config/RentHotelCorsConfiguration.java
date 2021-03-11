0c82f7018d65dcb7359ef6db8034efe8e4ee0f07
qbj <qbj5201314.yunfd.com>
2021-03-11 15:45:30 +0800

    提交代码，图片上传模块

--- rentHotel-upload/src/main/java/com/rentHotel/upload/config/RentHotelCorsConfiguration.java
+++ rentHotel-upload/src/main/java/com/rentHotel/upload/config/RentHotelCorsConfiguration.java
@@ -15,6 +15,7 @@ public class RentHotelCorsConfiguration {
         //允许跨域的域名；如果要携带cookie；就不能写* ；*代表所有的域名都可以跨域访问
         configuration.addAllowedOrigin("http://qian.rentHoter.com");
         configuration.addAllowedOrigin("http://localhost:6221");
+        configuration.addAllowedOrigin("http://localhost:2263");
         configuration.setAllowCredentials(true);//允许携带cookie
         configuration.addAllowedMethod("*");//代表所有的请求方法:GET、POST、PUT等
         configuration.addAllowedHeader("*");
